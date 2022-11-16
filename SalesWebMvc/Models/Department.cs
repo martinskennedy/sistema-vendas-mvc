c465d4654ca4b1aaad09003f6a3446b515641136
Kennedy Martins <kemartins@gmail.com>
2022-11-16 15:44:36 -0300

    Seeding Service

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -14,9 +14,8 @@ namespace SalesWebMvc.Models
         {
         }
 
-        public Department(int id, string name)
+        public Department(string name)
         {
-            Id = id;
             Name = name;
         }
 
